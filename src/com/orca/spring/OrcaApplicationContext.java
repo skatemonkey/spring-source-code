edcb69e6158ade7a74152301a885cca024527ecd
Hello World <56444800+skatemonkey@users.noreply.github.com>
2026-07-15 09:48:22 +0800

    Stage 2: Component Scanning — scan the configured package and find its classes

--- src/com/orca/spring/OrcaApplicationContext.java
+++ src/com/orca/spring/OrcaApplicationContext.java
@@ -1,10 +1,50 @@
 package com.orca.spring;
 
+import java.io.File;
+import java.net.URL;
+
 public class OrcaApplicationContext {
     private Class configClass;
 
     public OrcaApplicationContext(Class configClass) {
         this.configClass = configClass;
+
+        // Scanning
+        if (configClass.isAnnotationPresent(ComponentScan.class)) {
+            ComponentScan componentScanAnnotation = (ComponentScan) configClass.getAnnotation(ComponentScan.class);
+
+            String path = componentScanAnnotation.value(); // com.orca.service
+            path = path.replace(".", "/"); // com/orca/service
+
+            ClassLoader classLoader = OrcaApplicationContext.class.getClassLoader();
+            URL resource = classLoader.getResource(path); // file:/V:/.../com/orca/service
+
+            File file = new File(resource.getFile()); // V:\...\com\orca\service
+
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+
+                for (File f : files) {
+                    String fileName = f.getAbsolutePath(); // V:\...\com\orca\service\UserService.class
+
+                    if (fileName.endsWith(".class")) {
+                        String className = fileName.substring(fileName.indexOf("com"), fileName.indexOf(".class")); // com\orca\service\UserService
+                        className = className.replace("\\", "."); // com.orca.service.UserService
+
+                        try {
+                            Class<?> clazz = classLoader.loadClass("");
+                            if (clazz.isAnnotationPresent(Component.class)) {
+                                // Bean
+
+                            }
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+        }
     }
 
     public Object getBean(String beanName) {
