28a10eb0f2c7275583623197763425517890e6c4
Hello World <56444800+skatemonkey@users.noreply.github.com>
2026-07-15 10:24:08 +0800

    Stage 5: Bean Creation — instantiate bean objects using reflection

--- src/com/orca/spring/OrcaApplicationContext.java
+++ src/com/orca/spring/OrcaApplicationContext.java
@@ -1,6 +1,7 @@
 package com.orca.spring;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -74,7 +75,20 @@ public class OrcaApplicationContext {
     }
 
     private Object createBean(String beanName, BeanDefinition beanDefinition) {
-        return null;
+        Class clazz = beanDefinition.getType();
+        try {
+            Object instance = clazz.getConstructor().newInstance();
+
+            return instance;
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Object getBean(String beanName) {
